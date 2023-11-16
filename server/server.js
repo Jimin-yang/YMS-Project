09be4b8daffe441c1192d91ce2c8772511073f51
Daijin-cat <shirakami653@gmail.com>
2023-11-16 18:38:35 +0900

    server.js에 db 연동 및 연동 테스트용으로 콘솔에 db 테이블명 한번 표기되게함. (현재 서버로 구동시 문제 없음 확인)

--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// 데이터베이스 연결
+let db = new sqlite3.Database('../DataBase/movieDB.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the movieDB database.');
+});
+
+// 데이터베이스 테이블 조회 (테스트용_콘솔에 테이블 표시됨)
+db.all("SELECT name FROM sqlite_master WHERE type='table'", [], (err, tables) => {
+  if (err) {
+    throw err;
+  }
+  tables.forEach((table) => {
+    console.log(table.name);
+  });
+});
 
 // 정적 파일을 제공할 디렉토리 설정
 app.use(express.static(path.join(__dirname, '../build')));
